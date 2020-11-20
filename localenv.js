2a1bbbf918ff99bb2dc0c213f2287b5b4280ad6f
Thor-Eivind Nilsen <70974719+Thoren18@users.noreply.github.com>
2020-11-20 14:50:35 +0100

    css div fix

---
+++ localenv.js
@@ -0,0 +1,3 @@
+module.exports.credentials = "postgres://autaumpsjapjlv:2f4eb4699f86960c1b7010fa9c1b3797423c641d5e00a9e466804972f3f0ffc6@ec2-34-251-118-151.eu-west-1.compute.amazonaws.com:5432/d26oa5p5kgiart";
+module.exports.hashSecret = "ChristianRobereSimonsen";
+module.exports.tokenSecret = "vOVH6sdmpNWjRRIqCc7rdxs01lwHzfr3";
